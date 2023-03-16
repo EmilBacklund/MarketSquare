be5e2c826a9b2d7cdff7f0717e047ae10497c141
Emil Backlund <emil_backi_@hotmail.com>
2023-03-16 19:57:47 +0100

    trying Ant Design

--- day-2-single-product-view-start/src/components/views/SingleProduct.js
+++ day-2-single-product-view-start/src/components/views/SingleProduct.js
@@ -1,20 +1,35 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSingleProduct } from '../../store/modules/products';
+import { Image } from 'antd';
 
 const SingleProduct = () => {
   let { id } = useParams();
   const dispatch = useDispatch();
   const { singleProduct } = useSelector((state) => state.products);
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     dispatch(fetchSingleProduct(id));
   }, [dispatch, id]);
 
   return (
-    <div>
-      <h1>Hello I am single product view {id}</h1>
+    <div className='grid place-items-center h-screen p-5'>
+      <Image
+        preview={{ visible: false }}
+        width={200}
+        src={singleProduct.images[0]}
+        onClick={() => setVisible(true)}
+      />
+      <div style={{ display: 'none' }}>
+        <Image.PreviewGroup preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}>
+          <Image src={singleProduct.images[0]} />
+          <Image src={singleProduct.images[1]} />
+          <Image src={singleProduct.images[2]} />
+          <Image src={singleProduct.images[3]} />
+        </Image.PreviewGroup>
+      </div>
     </div>
   );
 };
