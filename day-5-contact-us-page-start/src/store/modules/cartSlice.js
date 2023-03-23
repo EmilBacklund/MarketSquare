37254f68aeeb2feb2245782b15c0f36ca6e9eb62
emil backlund <emil_backi_@hotmail.com>
2023-03-23 15:32:20 +0100

    day 5 complete

--- day-5-contact-us-page-start/src/store/modules/cartSlice.js
+++ day-5-contact-us-page-start/src/store/modules/cartSlice.js
@@ -1,33 +1,39 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState: {
-        productsInCart: [],
-        numberOfProductsInCart: 0,
+  name: 'cart',
+  initialState: {
+    productsInCart: [],
+    numberOfProductsInCart: 0,
+  },
+  reducers: {
+    // here we write the functions which will update the state
+    ADD_PRODUCT_TO_CART: (state, action) => {
+      // TODO i will update my productsInCart
+      console.log('action: ', action);
+      state.productsInCart = [...state.productsInCart, action.payload];
+      state.numberOfProductsInCart = state.productsInCart.length;
     },
-    reducers: { // here we write the functions which will update the state
-        ADD_PRODUCT_TO_CART: (state, action) => {
-            // TODO i will update my productsInCart
-            console.log("action: ",action);
-            state.productsInCart = [...state.productsInCart, action.payload];
-            state.numberOfProductsInCart = state.productsInCart.length
-        }
-    }
-})
+    REMOVE_SINGLE_PRODUCT: (state, action) => {
+      console.log(state, action);
+
+      state.numberOfProductsInCart = state.productsInCart.length;
+    },
+  },
+});
 
 export default cartSlice.reducer;
 
 // Actions
-const {ADD_PRODUCT_TO_CART} = cartSlice.actions;
-
+const { ADD_PRODUCT_TO_CART, REMOVE_SINGLE_PRODUCT } = cartSlice.actions;
 
 export const addSingleProductToCart = (productData) => (dispatch) => {
-    console.log("productData: ", productData);
-    dispatch(ADD_PRODUCT_TO_CART(productData))
-    // API CALL TO add product to the user cart in the BE
-}
+  console.log('productData: ', productData);
+  dispatch(ADD_PRODUCT_TO_CART(productData));
+  // API CALL TO add product to the user cart in the BE
+};
 
-
-
-
+export const removeSingleProductFromCart = (productId) => (dispatch) => {
+  console.log(productId);
+  dispatch(REMOVE_SINGLE_PRODUCT(productId));
+};
